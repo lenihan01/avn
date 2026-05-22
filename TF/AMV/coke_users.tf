60b8b02e9f31a4ca0d854f1101641b086eaf7b33
John Lenihan <john.lenihan@hpe.com>
2026-05-22 08:35:07 +0100

    aeven4

--- TF/AMV/coke_users.tf
+++ TF/AMV/coke_users.tf
@@ -1,13 +1,32 @@
-resource "hpe_morpheus_user" "coke" {
-  count                       = 1
+resource "hpe_morpheus_user" "coke1" {
   tenant_id                   = hpe_morpheus_tenant.coke-master-tenant.id 
-  username                    = "coke${count.index}"
-  email                       = "coke${count.index}@testacc.com"
+  username                    = "coke1"
+  email                       = "coke1@testacc.com"
   password_wo                 = "Secret123!"
   password_wo_version         = 1
   role_ids                    = [1]
   first_name                  = "Coke"
-  last_name                   = "User${count.index}"
+  last_name                   = "User1"
+  linux_username              = "linuser"
+  linux_password_wo           = "Linux123!"
+  linux_password_wo_version   = 1
+  linux_key_pair_id           = 100
+  receive_notifications       = false
+  windows_username            = "winuser"
+  windows_password_wo         = "Windows123!"
+  windows_password_wo_version = 1
+}
+
+
+resource "hpe_morpheus_user" "coke2" {
+  tenant_id                   = hpe_morpheus_tenant.coke-master-tenant.id
+  username                    = "coke2"
+  email                       = "coke2@testacc.com"
+  password_wo                 = "Secret123!"
+  password_wo_version         = 1
+  role_ids                    = [1]
+  first_name                  = "Coke"
+  last_name                   = "User2"
   linux_username              = "linuser"
   linux_password_wo           = "Linux123!"
   linux_password_wo_version   = 1
@@ -23,5 +42,5 @@ resource "hpe_morpheus_user_group" "example" {
   description  = "terraform"
   sudo_access  = true
   server_group = "test"
-  user_ids     = [hpe_morpheus_user.coke[count.index].id]
+  user_ids     = [hpe_morpheus_user.coke1.id, hpe_morpheus_user.coke2.id]
 }
