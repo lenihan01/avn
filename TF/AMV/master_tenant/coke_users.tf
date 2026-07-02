fc8654ebfdbf7dd80053bcfdc4c8fc1abcdfb607
John Lenihan <john.lenihan+hpeprod@hpe.com>
2026-07-02 14:07:03 +0100

    remove some hardwired usernames

--- TF/AMV/master_tenant/coke_users.tf
+++ TF/AMV/master_tenant/coke_users.tf
@@ -1,18 +1,18 @@
 resource "hpe_morpheus_user" "coke_admin" {
   tenant_id                   = hpe_morpheus_tenant.coke-master-tenant.id
-  username                    = "coke_admin"
-  email                       = "coke_admin@testacc.com"
+  username                    = var.coke_admin_username 
+  email                       = "${var.coke_admin_username}@testacc.com"
   password_wo                 = var.coke_admin_password 
   password_wo_version         = 1
   role_ids                    = [data.hpe_morpheus_role.coke_admin.id]
   first_name                  = "Coke"
   last_name                   = "Admin"
-  linux_username              = "coke_admin"
+  linux_username              = var.coke_admin_username 
   linux_password_wo           = var.coke_admin_password 
   linux_password_wo_version   = 1
   linux_key_pair_id           = 100
   receive_notifications       = false
-  windows_username            = "coke_admin"
+  windows_username            = var.coke_admin_username 
   windows_password_wo         = var.coke_admin_password 
   windows_password_wo_version = 1
   provider                    = hpe.master-tenant
