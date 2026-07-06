f0890be390d2f3064943dade942ab3d96de92b2d
John Lenihan <john.lenihan+hpeprod@hpe.com>
2026-07-06 18:00:48 +0100

    layout V

--- TF/AMV/master_tenant/coke_layouts.tf
+++ TF/AMV/master_tenant/coke_layouts.tf
@@ -4,11 +4,11 @@ data "hpe_morpheus_instance_type_layout" "hvm" {
 }
 
 resource "hpe_morpheus_instance_type_layout" "ubuntu_wordpress_vmware_layout" {
-  instance_type_id = hpe_morpheus_instance_type.ubuntu.id
+  instance_type_id = hpe_morpheus_instance_type.wordpress_ubuntu.id
   name             = "Ubuntu Wordpress VMWare Layout"
   labels           = ["coke", "layout", "terraform"]
   version          = "1.0"
   technology       = "vmware"
-  node_type_ids    = [hpe_morpheus_node_type.ubuntu_2404_vmware.id]
+  node_type_ids    = [data.hpe_morpheus_node_type.ubuntu_2404_vmware.id]
   workflow_id      = hpe_morpheus_workflow_provisioning.wordpress_workflow.id
 }
