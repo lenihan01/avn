72e056280e71968772f8e5923b07c6d30fdabdfc
John Lenihan <john.lenihan+hpeprod@hpe.com>
2026-07-06 15:08:59 +0100

    tasks V

---
+++ TF/AMV/master_tenant/pepsi_tasks.tf
@@ -0,0 +1,20 @@
+resource "hpe_morpheus_task_shell_script" "pepsi_shell_task_1" {
+  name                = "Pepsi Shell Task 1"
+  code                = "pepsishelltask1"
+  labels              = ["pepsi", "terraform"]
+  source_type         = "local"
+  script_content      = <<EOF
+  echo "Pepsi task testing"
+EOF
+  sudo                = true
+  retryable           = true
+  retry_count         = 1
+  retry_delay_seconds = 10
+  allow_custom_config = true
+#  visibility          = "public"
+  provider = hpe.pepsi-master-tenant
+  depends_on = [
+    hpe_morpheus_tenant.pepsi-master-tenant
+  ]
+}
+
