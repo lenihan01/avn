79afc224a10d2f400fc35c38251165468a356e7a
John Lenihan <john.lenihan+hpeprod@hpe.com>
2026-07-06 17:52:21 +0100

    layout VIII

--- TF/AMV/master_tenant/coke_workflows.tf
+++ TF/AMV/master_tenant/coke_workflows.tf
@@ -4,10 +4,8 @@ resource "hpe_morpheus_workflow_provisioning" "wordpress_workflow" {
   labels      = ["demo", "terraform"]
   platform    = "all"
   visibility  = "private"
-  task        = [
-    {
-      "task_id": hpe_morpheus_task_ansible_playbook.wordpress_ubuntu.id,
-      "task_phase": "provision"
-    }  
-  ]
+  task { 
+    "task_id": hpe_morpheus_task_ansible_playbook.wordpress_ubuntu.id
+    "task_phase": "provision"
+  }  
 }
