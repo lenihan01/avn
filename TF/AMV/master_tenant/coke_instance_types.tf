bfbb175abc0be734c680480597bbe4c20755171f
John Lenihan <john.lenihan+hpeprod@hpe.com>
2026-07-06 17:57:12 +0100

    instance type II

--- TF/AMV/master_tenant/coke_instance_types.tf
+++ TF/AMV/master_tenant/coke_instance_types.tf
@@ -2,3 +2,32 @@ data "hpe_morpheus_instance_type" "ubuntu" {
   name        = "Ubuntu"
   provider    = hpe.master-tenant
 }
+
+
+resource "hpe_morpheus_instance_type" "wordpress_ubuntu" {
+  name               = "Wordpress Ubuntu"
+  code               = "wordpress_ubuntu"
+  description        = "Wordpress Ubuntu Instance Toype"
+  labels             = ["coke", "instance", "terraform", "wordpress"]
+  category           = "web"
+  visibility         = "private"
+  image_path         = "tfexample.png"
+  image_name         = "tfexample.png"
+  featured           = false
+  enable_deployments = true
+  enable_scaling     = true
+  enable_settings    = true
+  environment_prefix = "TFEXAMPLE_DEMO"
+
+  evar {
+    name   = "first"
+    value  = "first"
+    export = true
+  }
+
+  evar {
+    name         = "second"
+    masked_value = "second"
+    export       = false
+  }
+}
