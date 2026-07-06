7d90ee02375eb525ec12e86fc45c20270050005a
John Lenihan <john.lenihan+hpeprod@hpe.com>
2026-07-06 14:14:51 +0100

    coke task I

---
+++ TF/AMV/master_tenant/coke_tasks.tf
@@ -0,0 +1,16 @@
+resource "hpe_morpheus_task_shell_script" "coke_shell_task_1" {
+  name                = "Coke Shell Task 1"
+  code                = "cokeshelltask1"
+  labels              = ["coke", "terraform"]
+  source_type         = "local"
+  script_content      = <<EOF
+  echo "Coke task testing"
+EOF
+  sudo                = true
+  retryable           = true
+  retry_count         = 1
+  retry_delay_seconds = 10
+  allow_custom_config = true
+  provider = hpe.coke-master-tenant
+}
+
